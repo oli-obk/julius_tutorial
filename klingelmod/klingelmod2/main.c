f44fbb8d3de7d0e8b58e3afe5ab642fd87b2d701
derirremitderwaffe <julius@julius-obk.de>
2013-02-13 13:48:17 +0100

    klingelmod funktionen ueberarbeitet

--- klingelmod/klingelmod2/main.c
+++ klingelmod/klingelmod2/main.c
@@ -20,41 +20,80 @@
 #define UART_BAUD_RATE 9600
 
 
+struct UART_KOMMANDOS
+            {
+                char *comandname;
+                int (*comandFunction)(void);
+            };
+
+struct UART_KOMMANDOS FunctionTable[10] =
+             {
+                 {"dial", &dial},
+                 {"opendoor", &opendoor},
+                 {"lock", &lock},
+                 {"unlock", &unlock}
+                };
+char dialchars[] = {
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+        '0',
+        '*',
+        '#'
+        };
+
+// buffer und counter fuer eingabe
+char uart_input_buffer[25] = {};
+char uart_input_breaker[] = {'\n', '\r'};
+int uart_input_counter = 0;
+
+
+//eingabe auf breaker checken, wenn nicht zeichen in buffer schreiben
+void kommando_check(char input, char* buffer, char* breaker, int counter)
+            {
+                if ( input != breaker[0] && c != breaker[1] && counter < 25 )
+                    {
+                    input = buffer[counter];
+                    ++counter;
+                    }
+
+                //bei breaker, buffer mit kommando array vergleichen
+
+                else {
+                    int uibc = 0;
+                    if ( input == breaker[uibc] )
+                    {
+                        ++uibc;
+                        if ( uibc == 2 )
+                        {
+                            uibc = 0;
+                            counter = 0;
+                            int i;
+                            for ( i = 0; i < (sizeof(FunctionTable) / sizeof(*FunctionTable)); i++)
+                            {
+                                if(strcmp(FunctionTable[i].comandname,uart_input_buffer)==0)
+                                {
+                                    FunctionTable[i].comandFunction();
+                                    break;
+                                }
 
 
 
-int main(void)
-{
+                            }
+                        }
+                    }
+                }
+            };
 
-    // Insert code
-
-    /*
-     * Intialize UART library, pass baudrate and AVR cpu clock
-     * with macro
-     * UART_BAUD_SELECT() (normal speed mode)
-     * of
-     * UART_BAUD_SELECT_DOUBLE_SPEED() (double speed mode)
-     */
-    uart_init( UART_BAUD_SELECT(UART_BAUD_RATE,F_CPU) );
-
-    /*
-     * now enable interrupt, since UART library is interrupt controlled
-     */
-
-    sei();
-
-    /*
-     * transmit string to uart
-     * the string is buffered by the uart library is a circular buffer
-     * and one character at a time is transmitted to the UART using interrupts.
-     * uart_puts() blocks if it can not write the whole string to the circular
-     * buffer
-     */
-
-    char c;
-
-    while(1)
+char getinputfromuart()
     {
+        char c;
         c = uart_getc();
         if (c & UART_NO_DATA )
         {
@@ -90,16 +129,34 @@ int main(void)
                  */
                 uart_puts_P("Buffer overflow error: ");
             }
-            /* check if transcived is an command
-             */
+            return c;
+    }
 
-             void getnumbers(char* arr, int num)
+void getnumbers(char* arr, int num)
              {
-
-             }
+                char number = getinputfromuart();
+                int gnc;
+                uart_input_counter = 0;
+                for ( gnc = 0; gnc < (sizeof(dialchars) / sizeof(*dialchars)); i++)
+                    {
+                        if( number == dialchars[gnc] && uart_input_counter != 24 )
+                            {
+                                uart_input_buffer[uart_input_counter] = number;
+                                uart_input_counter++;
+                            }
+                        else if ( number == uart_input_breaker[0] || number == uart_input_breaker[1] )
+                            {
+                                break;
+                            }
+                        else
+                            {
+                                break;
+                            }
+                    }
+             };
 
             //commandos welche ueber uart ausgefuehrt werden
-            int dial(void)
+int dial(void)
             {
                 char telefonnummer[25];
                 getnumbers(telefonnummer, 24);
@@ -115,81 +172,71 @@ int main(void)
                     dtmf_out(telefonnummer);
                     }
             };
-            int opendoor(void)
+
+
+int opendoor(void)
             {
                 void;
             };
 
 
-            int lock(void)
+int lock(void)
             {
                 void;
             };
 
 
-            int unlock(void)
+int unlock(void)
             {
                 void;
             };
 
 
-            struct UART_KOMMANDOS
-            {
-                char *comandname;
-                int (*comandFunction)(void);
-            };
-            struct UART_KOMMANDOS FunctionTable[10] =
-             {
-                 {"dial", &dial},
-                 {"opendoor", &opendoor},
-                 {"lock", &lock},
-                 {"unlock", &unlock}
-                };
 
 
 
-                // buffer und counter fuer eingabe
-                char uart_input_buffer[25] = {};
-                char uart_input_breaker[] = {'\n', '\r'};
+int main(void)
+{
+
+    // Insert code
+
+    /*
+     * Intialize UART library, pass baudrate and AVR cpu clock
+     * with macro
+     * UART_BAUD_SELECT() (normal speed mode)
+     * of
+     * UART_BAUD_SELECT_DOUBLE_SPEED() (double speed mode)
+     */
+    uart_init( UART_BAUD_SELECT(UART_BAUD_RATE,F_CPU) );
+
+    /*
+     * now enable interrupt, since UART library is interrupt controlled
+     */
+
+    sei();
+
+    /*
+     * transmit string to uart
+     * the string is buffered by the uart library is a circular buffer
+     * and one character at a time is transmitted to the UART using interrupts.
+     * uart_puts() blocks if it can not write the whole string to the circular
+     * buffer
+     */
 
 
-                int uart_input_counter = 0;
+
+    while(1)
+    {
+
+            /* check if transcived is an command
+             */
+
+
 
                 //uart eingabe checken
 
-                //eingabe auf breaker checken, wenn nicht zeichen in buffer schreiben
-                if ( c != uart_input_breaker[0] && c != uart_input_breaker[1] && uart_input_counter < 25 )
-                {
-                    c = uart_input_buffer[uart_input_counter];
-                    ++uart_input_counter;
-                }
+                kommando_check(uart_input_buffer, uart_input_buffer, uart_input_counter);
 
-                //bei breaker, buffer mit kommando array vergleichen
-
-                else {
-                    int uibc = 0;
-                    if ( c == uart_input_breaker[uibc] )
-                    {
-                        ++uibc;
-                        if ( uibc == 2 )
-                        {
-                            uibc = 0;
-                            uart_input_counter = 0;
-                            int i;
-                            for ( i = 0; i < (sizeof(FunctionTable) / sizeof(*FunctionTable)); i++)
-                            {
-                                if(strcmp(FunctionTable[i].comandname,uart_input_buffer)==0)
-                                {
-                                    FunctionTable[i].comandFunction();
-                                    break;
-                                }
-
-
-
-                            }
-                        }
-                    }
-                }
 
 
 
